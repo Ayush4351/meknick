ca684639703e86f5cb1022372f900f58f256be84
Ayush <ayush4351@gmail.com>
2020-11-06 01:22:27 +0530

    Preparing the Hero section

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 const app = angular.module("app", []);
+
+//Header Controller
+app.controller('headerController', ($scope) => {
+    $scope.class = "header";
+    $scope.changeClass = () => {
+        if ($scope.class === "header") {
+            $scope.class = "header menu-opened";
+        } else {
+            $scope.class = "header";
+        }
+    }
+})
