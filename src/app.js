92251b0add59993505d339c689abea5719a71770
Ayush <ayush4351@gmail.com>
2020-11-08 01:04:54 +0530

    Service section added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const app = angular.module("app", []);
+const app = angular.module("app", ['ngMessages', '720kb.datepicker']);
 
 //Header Controller
-app.controller('headerController', ($scope) => {
+app.controller('headerController', ['$scope', ($scope) => {
     $scope.class = "header";
     $scope.changeClass = () => {
         if ($scope.class === "header") {
@@ -10,4 +10,8 @@ app.controller('headerController', ($scope) => {
             $scope.class = "header";
         }
     }
-})
+}])
+
+app.controller('appointmentController', ['$scope', ($scope) => {
+
+}])
